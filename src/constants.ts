d5e7e2e5624fa68499f1d9d1bf62199f740bb4fd
Imam Janjua <i.janjua@simplicity.ag>
2025-10-12 20:10:43 +0200

    email for testing

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const EMAIL_SENDER = "info@codedecor.de";
 export const EMAIL_RECIPIENTS_PROD = ["app@meterstein.de"];
 export const EMAIL_RECIPIENTS_DEV = ["imamofficaly7@gmail.com"];
-export const EMAIL_RECIPIENTS = process.env.NODE_ENV === 'production' ? EMAIL_RECIPIENTS_PROD : EMAIL_RECIPIENTS_DEV;
+// export const EMAIL_RECIPIENTS = process.env.NODE_ENV === 'production' ? EMAIL_RECIPIENTS_PROD : EMAIL_RECIPIENTS_DEV;
+export const EMAIL_RECIPIENTS = EMAIL_RECIPIENTS_DEV;
