7d53872a76cf044e5e6f5ce1a2ece2578aeeaeb8
Imam Janjua <i.janjua@simplicity.ag>
2025-10-24 19:14:01 +0200

    domain chnaged

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const EMAIL_SENDER = "info@codedecor.de";
+export const EMAIL_SENDER = "mobileapp@meterstein.de";
 export const EMAIL_RECIPIENTS_PROD = ["app@meterstein.de"];
 export const EMAIL_RECIPIENTS_DEV = ["imamofficaly7@gmail.com"];
 export const EMAIL_RECIPIENTS = process.env.NODE_ENV === 'production' ? EMAIL_RECIPIENTS_PROD : EMAIL_RECIPIENTS_DEV;
